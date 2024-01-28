24342543fdc173fa5c3a8ff7ae076ffdfc8c594d
Elahe Hashemi <elahe.hs481@gmail.com>
2024-01-28 10:39:46 +0330

    fix redirect

--- app/auth.config.js
+++ app/auth.config.js
@@ -1,5 +1,5 @@
 export const authConfig = {
-  providers: [],
+  providers:[],
   pages: {
     signIn: "/login",
   },
@@ -7,14 +7,11 @@ export const authConfig = {
     authorized({ auth, request }) {
       const isLoggedIn = auth?.user;
       const isOnDashboard = request.nextUrl.pathname.startsWith("/dashboard");
-
       if (isOnDashboard) {
         if (isLoggedIn) return true;
         return false;
       } else if (isLoggedIn) {
-        return {
-          url: "/dashboard",
-        };
+        return Response.redirect(new URL("/dashboard", request.nextUrl));
       }
       return true;
     },
