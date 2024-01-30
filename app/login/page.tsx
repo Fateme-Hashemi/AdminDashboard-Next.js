dcaebd88a4fdca38124708be51ba43b2e2dcb025
Elahe Hashemi <elahe.hs481@gmail.com>
2024-01-30 12:58:39 +0330

    add info  paragraph

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -6,6 +6,7 @@ const LoginPage = () => {
   return (
     <div className={styles.login_container}>
       <LoginForm/>
+      <p className={styles.login_text}>use admin for username and password to login and then you can create your user</p>
     </div>
   );
 };
